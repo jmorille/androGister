d271f07b6919293d1fa8de7a7ba434efb6a67876
Jérôme Morille <jmorille@generali.fr>
2012-06-28 18:03:00 +0200

    Helper

--- src/eu/ttbox/androgister/model/PriceHelper.java
+++ src/eu/ttbox/androgister/model/PriceHelper.java
@@ -4,7 +4,7 @@ public class PriceHelper {
 
 	public static String getToStringPrice(long price) {
 		double priceInDevice = price /100d;
-		return String.format("%.,2f", priceInDevice);
+		return String.format("%,.2f", priceInDevice);
 	}
 	
 }
