a5f951abcc41637bcf7ca2dcff81b51a04aea081
jmorille <jmorille@generali.fr>
2012-07-13 23:16:38 +0200

    Pref

---
+++ src/eu/ttbox/androgister/ui/config/preference/EditTextSummaryPreference.java
@@ -0,0 +1,30 @@
+package eu.ttbox.androgister.ui.config.preference;
+
+import android.content.Context;
+import android.preference.EditTextPreference;
+import android.util.AttributeSet;
+
+public class EditTextSummaryPreference extends EditTextPreference {
+
+    public EditTextSummaryPreference(Context context) {
+        super(context);
+    }
+
+    public EditTextSummaryPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public EditTextSummaryPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setText(String text) {
+        String savedText = text;
+        if (text == null || text.trim().isEmpty()) {
+            savedText = null;
+        }
+        super.setText(savedText);
+        setSummary(text);
+    }
+
+}
