8427c36e986d210509ffa4a97bd7221ddf6df3c1
Jérôme Morille <jmorille@generali.fr>
2012-07-17 13:23:56 +0200

    ime

---
+++ src/eu/ttbox/androgister/ui/ime/BasketInputMethodService.java
@@ -0,0 +1,24 @@
+package eu.ttbox.androgister.ui.ime;
+
+import android.inputmethodservice.InputMethodService;
+import android.view.View;
+import eu.ttbox.androgister.R;
+
+/**
+ * {link http://developer.android.com/guide/topics/text/creating-input-method.html}
+ * @author a000cqp
+ *
+ */
+public class BasketInputMethodService extends InputMethodService {
+
+//    @Override 
+//    public View onCreateInputView() { 
+//        MyKeyboardView inputView = 
+//            (MyKeyboardView) getLayoutInflater().inflate( R.layout.input, null);
+//    
+//        inputView.setOnKeyboardActionListener(this); inputView.setKeyboard(mLatinKeyboard); 
+//        
+//        return mInputView; 
+//    } 
+    
+}
